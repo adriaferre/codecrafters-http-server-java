601e53fc7476af0294800234a3a849010f0629ad
Adrià Ferré <adria.ferre.97@gmail.com>
2024-04-01 17:15:11 +0200

    stage 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,15 +8,42 @@ public class Main {
      Socket clientSocket = null;
 
      try {
-       serverSocket = new ServerSocket(4221);
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
+         serverSocket = new ServerSocket(4221);
+         serverSocket.setReuseAddress(true);
+         clientSocket = serverSocket.accept(); // Wait for connection from client.
 
-       final OutputStream outputStream = clientSocket.getOutputStream();
-       outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+         handleConnection(clientSocket);
+
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
+  }
+
+  private static void handleConnection(final Socket clientSocket) {
+      try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+           OutputStream outputStream = clientSocket.getOutputStream())
+      {
+          final String request = bufferedReader.readLine();
+          String path = request.split(" ")[1];
+
+          if ("/".equals(path)) {
+              successfulResponse(outputStream);
+          } else {
+              notFoundResponse(outputStream);
+          }
+
+          outputStream.flush();
+      } catch (IOException e) {
+          System.out.println("exception occurred " + e.getMessage());
+          throw new RuntimeException(e);
+      }
+  }
+
+  private static void successfulResponse(final OutputStream outputStream) throws IOException {
+      outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+  }
+
+  private static void notFoundResponse(final OutputStream outputStream) throws IOException {
+      outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
   }
 }
